48f8da795d4d9c55cae6b4f3c9284374b5ecc5d8
erica0321 <hyunahrho29@gmail.com>
2024-12-09 19:02:07 +0900

    fix: 게시물 상세 페이지 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['d3fsuejlplp5xg.cloudfront.net'], // 외부 이미지 도메인 추가
+    domains: ['d3fsuejlplp5xg.cloudfront.net', 'localhost'],
   },
 }
 
--- src/app/posts/[id]/components/post/PostDetail.tsx
+++ src/app/posts/[id]/components/post/PostDetail.tsx
@@ -29,43 +29,44 @@ export default function PostDetail({
 }: PostDetailProps) {
   const data = responseData?.data
 
-  if (!data) {
-    return <div>게시글을 불러오는 중입니다...</div>
-  }
-
   return (
     <div className={styles.detailBoard}>
       <div className={styles.boardHeader}>
         <div className={styles.postType}>
-          {data.type === 'other' ? '고민' : '개발'}
+          {data?.type === 'other' ? '고민' : '개발'}
         </div>
         <div className={styles.writer}>
-          <ProfileImage image={data.profile_image} size={28} />
-          <p className={styles.postWriterName}>{data.nickname}</p>
+          {data?.profile_image && (
+            <ProfileImage image={data?.profile_image || ''} size={28} />
+          )}
+          <p className={styles.postWriterName}>{data?.nickname}</p>
           <div className={styles.postWriteDate}>
-            {changeDate(data.created_at)}
+            {changeDate(data?.created_at || '0')}
           </div>
         </div>
         <div className={styles.boardHeaderBottom}>
-          <span className={styles.detailBoardTitle}>{data.title}</span>
-          <PostButton postId={data.post_id} setIsPostDelete={setIsPostDelete} />
+          <span className={styles.detailBoardTitle}>{data?.title}</span>
+          <PostButton
+            postId={data?.post_id || 0}
+            setIsPostDelete={setIsPostDelete}
+          />
         </div>
       </div>
-      <PostAction view={data.view} comment={data.commentCount} />
+      <PostAction view={data?.view || 0} comment={data?.commentCount || 0} />
       <div className={styles.boardBody}>
-        {data.post_image && (
+        {data?.post_image && (
           <div className={styles.boardImageContainer}>
             <Image
               priority
               className={styles.boardImage}
-              src={data.post_image}
+              src={data?.post_image}
               alt='게시글 이미지'
               width={800} // 적절한 너비 설정
               height={600} // 적절한 높이 설정
             />
           </div>
         )}
-        <div className={styles.boardDetailContent}>{data.content}</div>
+        <div className={styles.boardDetailContent}>{data?.content}</div>
       </div>
     </div>
   )
